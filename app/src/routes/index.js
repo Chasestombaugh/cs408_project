6464e434f4e8bbc8e653e37ecc82f6cc9da08ef8
cstombaugh <chasestombaugh@u.boisestate.edu>
2026-03-14 14:24:56 -0600

    complete checkpoint 1 frontend skeleton and shared layout

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home (landing) page. */
+/* Landing page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Full Stack Starter Code' });
+  res.render('index', { title: 'RiftTracker' });
 });
+
+/* Match list page */
+router.get('/matches', function(req, res, next) {
+  res.render('matches', { title: 'Matches' });
+});
+
+/* Add match page */
+router.get('/matches/new', function(req, res, next) {
+  res.render('add-match', { title: 'Add Match' });
+});
+
+/* Match detail page */
+router.get('/matches/:id', function(req, res, next) {
+  res.render('match-detail', {
+    title: 'Match Details',
+    matchId: req.params.id
+  });
+});
+
+/* Stats page */
+router.get('/stats', function(req, res, next) {
+  res.render('stats', { title: 'Statistics' });
+});
+
 module.exports = router;
